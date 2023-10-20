b5a5cc6a561980447bcec22bc79f4f99cfad55da
LAPTOP-I80VHPF1\aldy aditiya <aldyaditiya@if.unjani.ac.id>
2023-10-20 21:20:58 +0700

    perubahan sesuai prinsip openclose

--- openclose/PremiumCinema.java
+++ openclose/PremiumCinema.java
@@ -3,5 +3,9 @@ package openclose;
 public class PremiumCinema {
     public PremiumCinema(double price) {
         this.price = price;
-        }
+    }
+    @Override
+    Double calculateAdminFee() {
+        return this.price * 12 / 100;
+    }
 }
