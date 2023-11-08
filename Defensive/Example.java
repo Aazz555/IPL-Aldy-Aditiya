4afbf2d4ba9d6add0e82099ce1de734bed7d3de8
LAPTOP-I80VHPF1\aldy aditiya <aldyaditiya@if.unjani.ac.id>
2023-11-08 11:22:59 +0700

    Example.java setelah di perbaiki

--- Defensive/Example.java
+++ Defensive/Example.java
@@ -1,12 +1,15 @@
+import java.util.Scanner;
+
 public class Example {
     public static void main(String[] args) {
         int age = 14;
-        assert age <= 18 : "Cannot Vote";
+        assert age >= 18 : "Cannot Vote";
         System.out.println("The voter's age is " + age);
+
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter ur age ");
+        System.out.print("Enter your age: ");
         int value = scanner.nextInt();
-        assert value >= 18 : " Not valid";
-        System.out.println("value is " + value);
+        assert value >= 18 : "Not valid";
+        System.out.println("Your age is " + value);
     }
 }
